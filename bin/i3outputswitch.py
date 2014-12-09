90cff4d0b41d2af3b84c07eb026d3ffbfef7158d
Modestas Stankus <modestas@wilibox.com>
2014-12-09 15:48:33 +0200

    i3 workspace switcher for dual monitor. xrand script

---
+++ bin/i3outputswitch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import i3
+import argparse
+
+workspaces = i3.get_workspaces()
+outputs = i3.get_outputs()
+
+def get_focused_workspace():
+    for w in workspaces:
+        if w["focused"]:
+            return w["name"], w["num"]
+
+def get_focused_screen():
+    for o in outputs:
+        if o['current_workspace'] and o['current_workspace'] == get_focused_workspace()[0]:
+            return o['name']
+
+def change_screen():
+    for o in outputs:
+        if o['current_workspace'] and o['current_workspace'] != get_focused_workspace()[0]:
+            i3.workspace(o['current_workspace'])
+            break
+
+def workspace_change(num):
+    num = int(num)
+    if get_focused_screen() != "HDMI2":
+        num = num + 5
+    i3.workspace(str(num))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''switch for i3''')
+    parser.add_argument('-s', '--change_screen', action='store_true')
+    parser.add_argument('-w', '--workspace_screen')
+
+
+    args = parser.parse_args()
+
+    if args.change_screen: change_screen()
+    if args.workspace_screen: workspace_change(args.workspace_screen)
---
+++ bin/xr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  xr.sh
+# 
+#         USAGE:  ./xr.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:   (), 
+#       COMPANY:  
+#       VERSION:  1.0
+#       CREATED:  04/30/2012 08:48:16 AM EEST
+#      REVISION:  ---
+#===============================================================================
+#senas monikas
+#xrandr --output HDMI2 --mode 1920x1200 --pos 0x0 --rotate normal --primary --output HDMI1 --mode 1680x1050 --left-of HDMI2
+#naujas monikas
+xrandr --output HDMI2 --mode 1920x1200 --pos 0x0 --rotate normal --primary --output HDMI1 --mode 1920x1200 --left-of HDMI2
+#naujas + persuktas
+#xrandr --output HDMI2 --mode 1920x1200 --pos 0x0 --rotate left --primary --output HDMI1 --mode 1920x1200 --left-of HDMI2
