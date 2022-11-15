7bc324ce98df8355a54a2fa033f097ce254688ce
Modestas Stankus <modestas.stankus@peplink.com>
2022-11-15 09:54:52 +0200

    i3: update scipt after python module update

--- bin/i3outputswitch.py
+++ bin/i3outputswitch.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
-import i3
+import i3ipc
 import argparse
+
+i3 = i3ipc.Connection()
 
 workspaces = i3.get_workspaces()
 outputs = i3.get_outputs()
 
 def get_focused_workspace():
     for w in workspaces:
-        if w["focused"]:
-            return w["name"], w["num"]
+        if w.focused:
+            return w.name, w.num
 
 def get_focused_screen():
     for o in outputs:
-        if o['current_workspace'] and o['current_workspace'] == get_focused_workspace()[0]:
-            return o['name']
+        print(o.name)
+        if o.current_workspace and o.current_workspace == get_focused_workspace()[0]:
+            return o.name
 
 def change_screen():
     for o in outputs:
@@ -25,13 +28,12 @@ def workspace_change(num):
     num = int(num)
     if get_focused_screen() == "HDMI1":
         num = num + 5
-    i3.workspace(str(num))
+    i3.command("workspace %s" % num)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''switch for i3''')
     parser.add_argument('-s', '--change_screen', action='store_true')
     parser.add_argument('-w', '--workspace_screen')
-
 
     args = parser.parse_args()
 
